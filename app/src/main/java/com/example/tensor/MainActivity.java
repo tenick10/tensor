1aa19ca5d8056231454c8c4daf25f0b0aa3b4a0d
kou0 <ialove1115>
2019-08-12 12:08:06 +0900

    first outline

--- app/src/main/java/com/example/tensor/MainActivity.java
+++ app/src/main/java/com/example/tensor/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tensor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //最初の画面を表示するメソッド呼び出し。
+        SetFirstScreen();
+
     }
+
+    private void SetFirstScreen() {
+
+        setContentView(R.layout.activity_main);
+        //条件ボタン
+        Button termbutton = findViewById(R.id.term);
+        //イベント追加
+        termbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //条件決定の画面へ
+                SetTermScreen();
+            }
+        });
+        //検索ボタン
+        Button searchbutton = findViewById(R.id.search);
+        //イベント追加
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //結果表示画面へ
+                SetResultScreen();
+            }
+        });
+    }
+    private void SetTermScreen() {
+        setContentView(R.layout.terms);
+        //初期画面へ戻るボタン
+        Button backtomainbutton = findViewById(R.id.backtomain);
+        backtomainbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //最初の画面へ
+                SetFirstScreen();
+            }
+        });
+    }
+    private void SetResultScreen() {
+        setContentView(R.layout.results);
+        //初期画面へ戻るボタン
+        Button backtomainbutton = findViewById(R.id.backtomain);
+        backtomainbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //最初の画面へ
+                SetFirstScreen();
+            }
+        });
+    }
+
 }
